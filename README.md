e626727feef77ba6488fcf3f0c8d125ff4abf130
Thue Janus Kristensen <thuejk@gmail.com>
2016-08-03 17:22:42 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+A python script to generate an atom feed from a phpBB subforum, for use in a web feed reader.
+
+For whatever reason, phpBB has buildin feeds for all new topic posts
+in all forums, and all posts (including in existant topics) in one
+subforum. But not a feed for only new topics in one subforum. This
+script created such a feed froma forum url.
+
+Usage: 
+       1) Put the script somewhere under your webserver root
+       2) Enable python scripting in your webserver configuration
+       3) Install the python packages mod_python, BeautifulSoup, and pyatom
+       4) Edit the "forumurl" variable in the script (grep for "writeme"), replacing it with a link to a phpBB subforum, e.g. "https://forums.factorio.com/viewforum.php?f=3"
+       5) Now the feed is available as www.example.com/phpbb_forum_topics_as_atom.py
+
+The script works by hackily parsing phpBB html, so it is possible that it will break
+or fail to work with some versions of phpBB
---
+++ phpbb_forum_topics_as_atom.py
@@ -0,0 +1,81 @@
+import urllib2
+from pyatom import AtomFeed
+import dateutil
+import re
+import mod_python
+import BeautifulSoup
+
+def get_favicon(soup, baseurl):
+   icon_link = soup.find("link", rel="shortcut icon")
+   if not icon_link:
+      icon_link = baseurl + "/favicon.ico"
+   #icon = urllib.urlopen(icon_link['href'])  #FIXME: maybe check for existence and favicon meta tag   
+   return icon_link
+
+def get_forumtitle(soup):
+   forumtitle = soup.find("title")
+   match = re.match("^(.*) &bull; View forum - (.*)$", forumtitle.contents[0])
+   if match:
+      forumtitle = match.group(1) + match.group(2)
+   else:
+      forumtitle = "(Forum title not found)"
+   return forumtitle
+
+def get_baseurl(forumurl):
+   urlmatch = re.match('https?://[^/]+', forumurl, re.I)
+   if urlmatch:
+      baseurl = urlmatch.group()
+   else:
+      req.status = mod_python.apache.HTTP_INTERNAL_SERVER_ERROR
+      raise Exception("Failed to extract base url from "+forumurl)
+   return baseurl
+
+def get_soup(forumurl):
+   try:
+      response = urllib2.urlopen(forumurl)
+      page = response.read()
+   except urllib2.HTTPError, error:
+      req.status = mod_python.apache.HTTP_INTERNAL_SERVER_ERROR
+      gerror = "Error returned by forum: " + error.read();
+      raise Exception("Failed to fetch forum source code. Is the forum at "+forumurl+" down? " + gerror)
+
+   soup = BeautifulSoup.BeautifulSoup(page)
+   return soup
+
+def index(req):
+   forumurl = "writeme" # replace with e.g. "https://forums.factorio.com/viewforum.php?f=3"
+   forumurl = "https://forums.factorio.com/viewforum.php?f=3"
+   if forumurl == "writeme":
+      req.status = mod_python.apache.HTTP_INTERNAL_SERVER_ERROR
+      return "The admin for this script needs to manually set the 'forumurl' parameter in the source code"
+
+   baseurl = get_baseurl(forumurl);
+   soup = get_soup(forumurl)
+
+   forumtitle = get_forumtitle(soup)
+
+   #generate feed
+   feed = AtomFeed(title=forumtitle,
+                   url=forumurl,
+                   icon=get_favicon(soup, baseurl))
+   #Add forum topics
+   for a in soup.findAll("a", { "class" : "topictitle" }):
+      datestring = a.parent.contents[-1]
+      datematch = re.match('^ &raquo; (.*?)\s*$', datestring, re.M)
+      datestring_trimmed = datematch.group(1)
+      published = updated = dateutil.parser.parse(datestring_trimmed)
+
+      author_a = a.parent.find("a", { "class" : "username-coloured"})
+      if author_a:
+         author = author_a.string
+      else:
+         author = "(author not found)"
+
+      feed.add(title=a.string,
+               url=baseurl + "/" + a["href"],
+               published=published,
+               updated=updated,
+               author=author,
+               )
+
+   return feed.to_string()
